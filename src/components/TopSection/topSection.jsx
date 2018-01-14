26cb4dcdbe37cd130eabb29c49a17eaae39a1dac
ksavransky-brightplan <ksavransky@brightplan.com>
2018-01-13 23:10:41 -0800

    menu good

--- src/components/TopSection/topSection.jsx
+++ src/components/TopSection/topSection.jsx
@@ -14,7 +14,7 @@ class TopSection extends Component {
         <div id='about-me'>
           <h1>Hi, my name is Konstantin.</h1>
           <h1>I'm a Full Stack Developer.</h1>
-          <h3>I love to code, have strong leadership and business sense, and a knack for design.</h3>
+          <h3>I love to code, have keen product sense, and a knack for design.</h3>
         </div>
       </div>
     )
