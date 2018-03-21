bd378a286c017f7058f996ac388c294a2bd2212d
ksavransky-brightplan <ksavransky@brightplan.com>
2018-03-21 07:42:55 -0700

    projects

--- src/components/ProjectsSection/projectsSection.jsx
+++ src/components/ProjectsSection/projectsSection.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Masonry from 'react-masonry-component'
+import HipstergramPhoto from '../../images/projects/hipstergram.png'
+import MarkovPhoto from '../../images/projects/markov.png'
+import PongPhoto from '../../images/projects/pong.png'
+import AlgoPhoto from '../../images/projects/algotracker.png'
 import './projectsSection.css'
 
 class ProjectsSection extends Component {
@@ -20,12 +24,28 @@ class ProjectsSection extends Component {
                 fitWidth: true
             }}>
             <div>
-              <div className='project-card'></div>
-              <div className='project-card'></div>
+              <div className='project-card'>
+                <h4>Hipstergram</h4>
+                <img src={HipstergramPhoto} />
+                <h5>React/Redux, Ruby on Rails, Postgres</h5>
+              </div>
+              <div className='project-card'>
+                <h4>Markov Tweets</h4>
+                <img src={MarkovPhoto} />
+                <h5>React/Redux, NodeJS, Twitter API</h5>
+              </div>
             </div>
             <div>
-              <div className='project-card'></div>
-              <div className='project-card'></div>
+              <div className='project-card'>
+                <h4>Super Pong</h4>
+                <img src={PongPhoto} />
+                <h5>Javascript, Canvas</h5>
+              </div>
+              <div className='project-card'>
+                <h4>AlgoTracker</h4>
+                <img src={AlgoPhoto} />
+                <h5>Javascript, D3</h5>
+              </div>
             </div>
         </Masonry>
       </div>
